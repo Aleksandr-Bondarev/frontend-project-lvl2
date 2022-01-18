544a91146011132ac63d55fe38c398e4d95e1cde
Aleksandr Bondarev <alexbondarev1331@mail.ru>
2022-01-19 00:16:27 +0300

    added .arguments and .option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,5 +4,7 @@ import { Command } from '../node_modules/commander/esm.mjs';
 const program = new Command();
 program
 	.version('0.0.1', '-v, --vers', 'output the current version')
-	.description('Compares two configuration files and shows a difference.');
+	.description('Compares two configuration files and shows a difference.')
+	.arguments('<firstArgument><secondArgument>')
+	.option('-f, --format [type]', 'output format');
 program.parse();
