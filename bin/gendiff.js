2867e9d21ebd8b134a2bfefb4ded5744a215061a
Aleksandr Bondarev <alexbondarev1331@mail.ru>
2022-01-18 21:31:46 +0300

    created bin/ with gendiff.js inside

---
+++ bin/gendiff.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+import { Command } from '../node_modules/commander/esm.mjs';
+const program = new Command();
+program
+	.version('0.0.1', '-v, --vers', 'output the current version')
+	.description('Compares two configuration files and shows a difference.');
+program.parse();
