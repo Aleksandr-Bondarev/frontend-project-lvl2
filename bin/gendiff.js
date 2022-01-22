12c52f478a0b04b48a3e1d5cb483505144ea2cf8
Aleksandr Bondarev <alexbondarev1331@mail.ru>
2022-01-22 15:56:13 +0300

    added arguments and action to gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 
+import _ from 'lodash';
+import gendiff from '../src/index.js';
 import { Command } from '../node_modules/commander/esm.mjs';
 const program = new Command();
 program
 	.version('0.0.1', '-v, --vers', 'output the current version')
 	.description('Compares two configuration files and shows a difference.')
-	.arguments('<firstArgument><secondArgument>')
-	.option('-f, --format [type]', 'output format');
-program.parse();
+	.option('-f, --format [type]', 'output format')
+	.arguments('<firstFilePath> <secondFilePath>')
+	.action((firstFilePath, secondFilePath) => {
+		gendiff(firstFilePath, secondFilePath);
+	});
+program.parse(process.argv);
