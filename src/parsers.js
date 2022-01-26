ff7bf22a365597324d1cc67f6f9eef23a8c51412
Aleksandr Bondarev <alexbondarev1331@mail.ru>
2022-01-26 12:55:49 +0300

    created parsers module

---
+++ src/parsers.js
@@ -0,0 +1,11 @@
+import yaml from 'js-yaml';
+
+const parsers = {
+  '.json': JSON.parse,
+  '.yml': yaml.load,
+  '.yaml': yaml.load,
+};
+
+const parse = (data, format) => parsers[format](data);
+
+export default parse;
