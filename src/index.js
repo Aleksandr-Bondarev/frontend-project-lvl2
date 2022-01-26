63a15d281ff8c2f4f2472686c7cbc7d5a461aba3
Aleksandr Bondarev <alexbondarev1331@mail.ru>
2022-01-26 13:29:31 +0300

    some fixes in index.js

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ const gendiff = (filepath1, filepath2) => {
     if (key in obj1 && !(key in obj2)) {
       result = result.concat(`  - ${key}: ${obj1[key]}\n`);
     }
-    if (key in obj2 && !(key in obj1)) {
+    else if (key in obj2) {
       result = result.concat(`  + ${key}: ${obj2[key]}\n`);
     }
   }
