523575430c2f21d39dc10602b41ade3e55006bdf
Aleksandr Bondarev <alexbondarev1331@mail.ru>
2022-01-26 12:55:03 +0300

    added new yaml test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -15,6 +15,14 @@ const jsonFile1 = getFixturePath('file1.json');
 const jsonFile2 = getFixturePath('file2.json');
 const expectedJson = readFile('expectedJson.txt');
 
+const ymlFile1 = getFixturePath('file1.yml');
+const ymlFile2 = getFixturePath('file2.yml');
+const expectedYml = readFile('expectedYml.txt');
+
 test('gendiff json', () => {
   expect(gendiff(jsonFile1, jsonFile2)).toEqual(expectedJson);
+});
+
+test('gendiff yml', () => {
+  expect(gendiff(ymlFile1, ymlFile2)).toEqual(expectedYml);
 });
