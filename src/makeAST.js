439b8f449dc88055b5b47cf28c90a1025c50cc53
Aleksandr Bondarev <alexbondarev1331@mail.ru>
2022-01-28 21:06:42 +0300

    created makeAST function

---
+++ src/makeAST.js
@@ -0,0 +1,38 @@
+import _ from 'lodash';
+
+const makeAST = (obj1, obj2) => {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  const unitedKeys = keys1.concat(keys2);
+  unitedKeys.sort();
+
+  const duplicateFreeKeys = _.sortedUniq(unitedKeys);
+
+  return duplicateFreeKeys.map((key) => {
+    if (!_.has(obj2, key)) {
+      return { name: key, type: 'removed', value: obj1[key] };
+    }
+    if (!_.has(obj1, key)) {
+      return { name: key, type: 'added', value: obj2[key] };
+    }
+    if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
+      return {
+        name: key,
+        type: 'nested',
+        children: makeAST(obj1[key], obj2[key]),
+      };
+    }
+    if (_.isEqual(obj1[key], obj2[key])) {
+      return { name: key, type: 'equal', value: obj1[key] };
+    }
+    return {
+      name: key,
+      type: 'updated',
+      oldValue: obj1[key],
+      newValue: obj2[key],
+    };
+  });
+};
+
+export default makeAST;
