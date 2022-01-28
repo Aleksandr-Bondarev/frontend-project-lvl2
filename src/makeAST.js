f476487ad510541437a1ee78f0db977496c925a9
Aleksandr Bondarev <alexbondarev1331@mail.ru>
2022-01-28 22:53:01 +0300

    some fixes in makeAST

--- src/makeAST.js
+++ src/makeAST.js
@@ -3,11 +3,7 @@ import _ from 'lodash';
 const makeAST = (obj1, obj2) => {
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
-
-  const unitedKeys = keys1.concat(keys2);
-  unitedKeys.sort();
-
-  const duplicateFreeKeys = _.sortedUniq(unitedKeys);
+  const duplicateFreeKeys = _.sortedUniq(keys1.concat(keys2).sort());
 
   return duplicateFreeKeys.map((key) => {
     if (!_.has(obj2, key)) {
@@ -17,11 +13,7 @@ const makeAST = (obj1, obj2) => {
       return { name: key, type: 'added', value: obj2[key] };
     }
     if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
-      return {
-        name: key,
-        type: 'nested',
-        children: makeAST(obj1[key], obj2[key]),
-      };
+      return { name: key, type: 'nested', children: makeAST(obj1[key], obj2[key]) };
     }
     if (_.isEqual(obj1[key], obj2[key])) {
       return { name: key, type: 'equal', value: obj1[key] };
