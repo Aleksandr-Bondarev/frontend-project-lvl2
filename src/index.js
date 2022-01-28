0fd01539f222cdb645ad1a3cab2796b3af3ddbac
Aleksandr Bondarev <alexbondarev1331@mail.ru>
2022-01-28 21:05:11 +0300

    new imports and logic for index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import _ from 'lodash';
 
 import parse from './parsers.js';
 
+import makeAST from './makeAST.js';
+
+import astDecoder from './formatters/stylish.js';
+
 const gendiff = (filepath1, filepath2) => {
   const fullPath1 = path.resolve(process.cwd(), filepath1);
   const fullPath2 = path.resolve(process.cwd(), filepath2);
@@ -19,32 +23,8 @@ const gendiff = (filepath1, filepath2) => {
   const obj1 = parse(data1, fileFormat1);
   const obj2 = parse(data2, fileFormat2);
 
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
-
-  const unitedKeys = keys1.concat(keys2);
-  unitedKeys.sort();
-
-  const duplicateFreeKeys = _.sortedUniq(unitedKeys);
-
-  let result = '{\n';
-  for (const key of duplicateFreeKeys) {
-    if (obj1[key] === obj2[key] && obj1[key] !== undefined) {
-      result = result.concat(`    ${key}: ${obj1[key]}\n`);
-    }
-    if (key in obj1 && key in obj2 && obj1[key] !== obj2[key]) {
-      result = result.concat(`  - ${key}: ${obj1[key]}\n`);
-      result = result.concat(`  + ${key}: ${obj2[key]}\n`);
-    }
-    if (key in obj1 && !(key in obj2)) {
-      result = result.concat(`  - ${key}: ${obj1[key]}\n`);
-    }
-    if (key in obj2 && !(key in obj1)) {
-      result = result.concat(`  + ${key}: ${obj2[key]}\n`);
-    }
-  }
-  result = result.concat('}');
-  return result;
+  const ast = makeAST(obj1, obj2);
+  return astDecoder(ast);
 };
 
 export default gendiff;
