7db310db2e42a53c1babc7490c16f1826000fd45
Aleksandr Bondarev <alexbondarev1331@mail.ru>
2022-01-28 21:08:16 +0300

    updated tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -8,21 +8,22 @@ import gendiff from '../src/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const getFixturePath = (filename) =>
+  path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+
+const expected = readFile('expected.txt');
 
 const jsonFile1 = getFixturePath('file1.json');
 const jsonFile2 = getFixturePath('file2.json');
-const expectedJson = readFile('expectedJson.txt');
 
 const ymlFile1 = getFixturePath('file1.yml');
 const ymlFile2 = getFixturePath('file2.yml');
-const expectedYml = readFile('expectedYml.txt');
 
 test('gendiff json', () => {
-  expect(gendiff(jsonFile1, jsonFile2)).toEqual(expectedJson);
+  expect(gendiff(jsonFile1, jsonFile2)).toEqual(expected);
 });
 
 test('gendiff yml', () => {
-  expect(gendiff(ymlFile1, ymlFile2)).toEqual(expectedYml);
+  expect(gendiff(ymlFile1, ymlFile2)).toEqual(expected);
 });
