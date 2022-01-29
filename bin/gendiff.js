d9b8af9e7993cfbf4aa33a30387712cbadd13019
Aleksandr Bondarev <alexbondarev1331@mail.ru>
2022-01-30 00:23:01 +0300

    fixes for working with format argument

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,9 +7,9 @@ const program = new Command();
 program
   .version('0.0.1', '-v, --vers', 'output the current version')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<firstFilePath> <secondFilePath>')
   .action((firstFilePath, secondFilePath) => {
-    console.log(gendiff(firstFilePath, secondFilePath));
+    console.log(gendiff(firstFilePath, secondFilePath, program.opts().format));
   });
 program.parse(process.argv);
