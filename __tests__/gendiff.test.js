c08b0d0209e7062e7abec0e86061011dafdcd0af
Aleksandr Bondarev <alexbondarev1331@mail.ru>
2022-01-30 00:27:19 +0300

    refactoring and added plain tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -8,10 +8,12 @@ import gendiff from '../src/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const getFixturePath = (filename) =>
+  path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-const expected = readFile('expected.txt');
+const expectedStylish = readFile('expectedStylish.txt');
+const expectedPlain = readFile('expectedPlain.txt');
 
 const jsonFile1 = getFixturePath('file1.json');
 const jsonFile2 = getFixturePath('file2.json');
@@ -19,10 +21,15 @@ const jsonFile2 = getFixturePath('file2.json');
 const ymlFile1 = getFixturePath('file1.yml');
 const ymlFile2 = getFixturePath('file2.yml');
 
-test('gendiff json', () => {
-  expect(gendiff(jsonFile1, jsonFile2)).toEqual(expected);
-});
-
-test('gendiff yml', () => {
-  expect(gendiff(ymlFile1, ymlFile2)).toEqual(expected);
-});
+const formatters = [
+  ['stylish', expectedStylish],
+  ['plain', expectedPlain],
+];
+for (const [formatter, expected] of formatters) {
+  test(`gendiff json ${formatter}`, () => {
+    expect(gendiff(jsonFile1, jsonFile2, formatter)).toEqual(expected);
+  });
+  test(`gendiff yml ${formatter}`, () => {
+    expect(gendiff(ymlFile1, ymlFile2, formatter)).toEqual(expected);
+  });
+}
