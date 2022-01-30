e62a4e908540d9be43924e7a32e57108ef77e7a6
Aleksandr Bondarev <alexbondarev1331@mail.ru>
2022-01-30 18:11:52 +0300

    removed sort() method from makeAST

--- src/makeAST.js
+++ src/makeAST.js
@@ -1,10 +1,9 @@
 import _ from 'lodash';
 
 const makeAST = (obj1, obj2) => {
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
-  const duplicateFreeKeys = _.sortedUniq(keys1.concat(keys2).concat().sort());
-
+  const duplicateFreeKeys = _.sortBy(
+    _.union(Object.keys(obj1), Object.keys(obj2)),
+  );
   return duplicateFreeKeys.map((key) => {
     if (!_.has(obj2, key)) {
       return { name: key, type: 'removed', value: obj1[key] };
