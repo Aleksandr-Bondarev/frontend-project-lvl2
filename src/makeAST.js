ce07044a9e2d2a1a7a65ed469f5fb98051114146
Aleksandr Bondarev <alexbondarev1331@mail.ru>
2022-01-30 18:01:25 +0300

    fixed sort() in makeAST function

--- src/makeAST.js
+++ src/makeAST.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 const makeAST = (obj1, obj2) => {
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
-  const duplicateFreeKeys = _.sortedUniq(keys1.concat(keys2).sort());
+  const duplicateFreeKeys = _.sortedUniq(keys1.concat(keys2).concat().sort());
 
   return duplicateFreeKeys.map((key) => {
     if (!_.has(obj2, key)) {
@@ -13,7 +13,11 @@ const makeAST = (obj1, obj2) => {
       return { name: key, type: 'added', value: obj2[key] };
     }
     if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
-      return { name: key, type: 'nested', children: makeAST(obj1[key], obj2[key]) };
+      return {
+        name: key,
+        type: 'nested',
+        children: makeAST(obj1[key], obj2[key]),
+      };
     }
     if (_.isEqual(obj1[key], obj2[key])) {
       return { name: key, type: 'equal', value: obj1[key] };
